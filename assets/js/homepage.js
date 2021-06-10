c55fd772953433e546a508b5ea856238aa2dc182
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-10 11:01:53 -0500

    conditions to name input

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,23 +1,32 @@
-var getUserRepos = function(user) {
+var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a get request to url
-  fetch(apiUrl).then(function(response) {
+  fetch(apiUrl).then(function (response) {
     console.log(response);
-    response.json().then(function(data) {
+    response.json().then(function (data) {
       console.log(data);
+
     });
 
-    
-var userFormEl = document.querySelector("#user-form");
-var NameInputEl = document.querySelector("#username");
-var formSubmitHandler = function (event) {
-  event.preverntDefault();
-  console.log(event);
+  });
+
 };
 
-  });
+var userFormEl = document.querySelector("#user-form");
+var NameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preverntDefault();
+  
+  //get value from input element
+  var username = nameInputEl.Value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value
+  }
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
