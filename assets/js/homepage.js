b3726111488ffe801a66f23aec052ef1e11a723f
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-11 16:16:43 -0500

    new variables at the 1-2 line in home js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
