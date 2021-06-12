39da0acf5c1c1afcd5b0efcebecc1055c9f8cd22
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-11 19:32:58 -0500

    catch error in fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,16 +6,22 @@ var getUserRepos = function(user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a get request to url
-  fetch(apiUrl).then(function(response) {
+  fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
     if (response.ok) {
       response.json().then(function(data) {
         displayRepos(data, user);
       });
     } else {
-      alert("Error: GitHub User Not Found");
+      alert('Error: GitHub User Not Found');
     }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
-
+  
   };
 
 
@@ -41,6 +47,11 @@ var formSubmitHandler = function(event) {
 
 
 var  displayRepos = function (repos, searchTerm) {
+  // check if api returned any repos
+if (repos.length === 0) {
+  repoContainerEl.textContent = "No repositories found.";
+  return;
+}
   // clear old content
 repoContainerEl.textContent = "";
 repoSearchTerm.textContent = searchTerm;
