7666decc81d1e1b94d24474cf29010a441f359ee
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-13 18:34:31 -0500

    if statement in siplay function

--- assets/js/single.js
+++ assets/js/single.js
@@ -17,6 +17,10 @@ var getRepoIssues = function(repo) {
     });
 
       var displayIssues = function(issues) {
+        if (issues.length ===0) {
+          issueContainerEl.textContent = "This repo has no open issues!";
+          return;
+        }
         for (var i = 0; i < issues.length; i++) {
           // create a link element to take users to the issue on github
           var issueEl = document.createElement("a");
