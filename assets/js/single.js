222f8592fd3198bc09d2c8a5d9c7215373efd57c
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-13 18:32:17 -0500

    adding issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -21,6 +23,29 @@ var getRepoIssues = function(repo) {
           issueEl.classList = "list-item flex-row justify-space-between align-center";
           issueEl.setAttribute("href", issues[i].html_url);
           issueEl.setAttribute("target", "_blank");
+
+          //create span to hold issue title
+          var titleEl = document.createElement("span");
+          titleEl.textContent = issues[i].title;
+
+          //append to container
+          issueEl.appendChild(titleEl);
+
+          //create a type element
+          var typeEl = document.createElement("span");
+
+          //check if issues is an actual issue or a pull request
+          if (issues[i].pull_request){
+            typeEl.textContent = "(Pull request)";
+
+          } else {
+            typeEl.textContent = "(Issue)";
+          }
+
+          //append to container
+          issueEl.appendChild(typeEl);
+
+          issueContainerEl.appendChild(issueEl);
         }
 
       };
