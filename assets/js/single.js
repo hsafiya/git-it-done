b8a557e5f9a70fd86e905f82ec7aa0afdbd69639
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-14 21:57:52 -0500

    conditional statement to getRepoNAme

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,12 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoName = function(){
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else{
+    document.location.replace("./index.html");
+  }
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
   getRepoIssues(repoName);
